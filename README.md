c860662fcff352ab5cecda3867d52e2595af6d8e
Thiago Mendes <thiagoromendes@gmail.com>
2021-03-23 10:47:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pratica-fundamentos-react
---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,29 @@
+import { RepositoryItem } from "./RepositoryItem";
+import { useEffect, useState } from "react";
+
+import '../styles/repositories.scss';
+
+interface Rerpository {
+    name: string
+    description: string;
+    html_url: string;
+}
+
+export function RepositoryList() {
+    const [repositories, setRepositories] = useState<Rerpository[]>([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos').
+        then(response => response.json()).
+        then(data => setRepositories(data))
+    },[])
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository}/>)}
+            </ul>
+        </section>
+    );
+}
